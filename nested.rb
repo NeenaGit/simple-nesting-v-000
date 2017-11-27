2bb8704223d030ded18804e8c923aee59c5fe7c9
Neena van de Water <nkuriakose@hotmail.com>
2017-11-27 17:11:22 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -122,7 +122,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
- # programmer_hash[:dennis_ritchie][:languages] << "Assembly"
- #
- # programmer_hash
+ new_language = "Assembly"     
+ programmer_hash[:dennis_ritchie][:languages] << new_language
+ 
+ programmer_hash
 end
